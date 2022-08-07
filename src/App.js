9c6d9fa02ac8962e05d4c1840bb2d5f4fb2a9195
Youssef Ayman <yosofaymanessawy@gmail.com>
2022-08-07 13:59:55 +0200

    Utilities Commit
    Set Utilities For Css That Will Be Used In whole App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
-import AppRouter from './Router';
+import "./App.css";
+import AppRouter from "./Router";
+import "./scss/main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
 import Home from './pages/Home'
 
 const AppRouter = () => {
   return (
     <Router>
+        <NavBar />
         <Routes>
             <Route element={<Home />} path="/"/>
         </Routes>
